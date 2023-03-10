84c465c96df66f3228a31041acbb80769cbdd9cd
Chuma Mukala <chumamukala@gmail.com>
2023-03-10 13:58:48 +0100

    Created controllers

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root to: 'lists#index'
+  get "lists/:id", to: "lists#show"
+  GET "lists/new", to: "lists#new"
+  resources :movies
+
+  # resources :lists do
+  #   resources :bookmarks
+  # end
 end
